d7faa7aa67c0246c6c363afe819c966ec3198321
6Morpheus6 <wanton-decay@gmx.net>
2026-05-08 08:46:36 +0200

    start GPT-SoVITS server

--- start.js
+++ start.js
@@ -10,13 +10,26 @@ module.exports = {
           HF_HUB_ENABLE_HF_TRANSFER: "1",
           TORCH_COMPILE_DISABLE: "1",
           PYTHONPATH: "TTS-Engines/CosyVoice",
-          TRANSLATE_BASE_URL: "http://localhost:11434/v1"
+          TRANSLATE_BASE_URL: "http://localhost:11434/v1",
+          OMNIVOICE_GPTSOVITS_URL: "http://127.0.0.1:9880"
         },
         venv: ".venv",
         path: "app",
         message: "python -m uvicorn main:app --app-dir backend --host 127.0.0.1 --port 3900",
         on: [{
           event: "/Application startup complete|Uvicorn running on/i",
+          done: true,
+        }],
+      },
+    },
+    {
+      method: "shell.run",
+      params: {
+        venv: "../../.venv",
+        path: "app/TTS-Engines/GPT-SoVITS",
+        message: "python api_v2.py -a 127.0.0.1 -p 9880 -c GPT_SoVITS/configs/tts_infer.yaml",
+        on: [{
+          event: "/(http:\\/\\/\\S+)/",
           done: true,
         }],
       },

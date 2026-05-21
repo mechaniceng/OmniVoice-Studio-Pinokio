d1aada6f1c85cf346eeba507fa3a62b46520f21a
6Morpheus6 <wanton-decay@gmx.net>
2026-05-21 03:28:06 +0200

    downgrade setuptools

--- install.js
+++ install.js
@@ -72,7 +72,7 @@ module.exports = {
         path: "app",
         message: [
           "uv sync",
-          "uv pip install voxcpm openai sherpa-onnx wheel wheel_stub setuptools==80.9.0"
+          "uv pip install voxcpm openai sherpa-onnx wheel wheel_stub setuptools==65.5.0"
         ]
       },
     },

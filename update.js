ded54a40e6f826762c585d634ec0ee2a5071450f
6Morpheus6 <wanton-decay@gmx.net>
2026-05-29 17:49:36 +0200

    update all TTS-Engines

--- update.js
+++ update.js
@@ -14,6 +14,38 @@ module.exports = {
         path: "app",
         message: "git pull",
       },
-    }
+    },
+    // Update MOSS-TTS-Nano
+    {
+      method: "shell.run",
+      params: {
+        path: "app/TTS-Engines/MOSS-TTS-Nano",
+        message: "git pull",
+      },
+    },
+    // Update CosyVoice
+    {
+      method: "shell.run",
+      params: {
+        path: "app/TTS-Engines/CosyVoice",
+        message: "git pull",
+      },
+    },
+    // Update CosyVoice submodules
+    {
+      method: "shell.run",
+      params: {
+        path: "app/TTS-Engines/CosyVoice",
+        message: "git submodule update --init --recursive",
+      },
+    },
+    // Update GPT-SoVITS
+    {
+      method: "shell.run",
+      params: {
+        path: "app/TTS-Engines/GPT-SoVITS",
+        message: "git pull",
+      },
+    },
   ]
 }
